fe41154582defbd304689db910b022cd6519c1a5
KKardd <wlstjd4013@gmail.com>
2023-07-19 02:31:21 +0900

    feat: apply datatype

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// const commitMessage = process.argv[2];
-// const notionKey = process.argv[3];
-// const databaseID = process.argv[4];
-
-// const {Client} = require("@notionhq/client");
-
-// const notion = new Client({auth: notionKey});
-
-// const date = new Date();
-// date.setHours(date.getHours() + 9);
-// async function addItem(text) {
-//     try {
-//         const response = await notion.pages.create({
-//             parent: {database_id: databaseID},
-//             properties: {
-//                 title: {
-//                     title: [
-//                         {
-//                             text: {
-//                                 content: text,
-//                             },
-//                         },
-//                     ],
-//                 },
-//                 Date: {
-//                     date: {
-//                         start: date.toISOString(),
-//                         time_zone: "Asia/Seoul",
-//                     },
-//                 },
-//             },
-//         });
-//         console.log("Success! Entry added.");
-//     } catch (error) {
-//         console.error(error.body);
-//     }
-// }
-
-// addItem(commitMessage);
-
-// const commitMessage = "commit message3";
-// const notionKey = "secret_xiU4MMKGoh6I9r0dopLsEV0ef2ep3JcKjrAuXVdPxMq";
-// const databaseID = "dffc1666e6894880bb89196d547a2750";
-
 const commitMessage = process.argv[2];
 const notionKey = process.argv[3];
 const databaseID = process.argv[4];
@@ -49,6 +5,9 @@ const databaseID = process.argv[4];
 const {Client} = require("@notionhq/client");
 
 const notion = new Client({auth: notionKey});
+
+const date = new Date();
+date.setHours(date.getHours() + 9);
 
 async function addItem(text) {
     try {
@@ -64,12 +23,12 @@ async function addItem(text) {
                         },
                     ],
                 },
-                // Date: {
-                //     date: {
-                //         start: date.toISOString(),
-                //         time_zone: "Asia/Seoul",
-                //     },
-                // },
+                Date: {
+                    date: {
+                        start: date.toISOString(),
+                        time_zone: "Asia/Seoul",
+                    },
+                },
             },
         });
         console.log(response);
@@ -78,5 +37,5 @@ async function addItem(text) {
         console.error(error.body);
     }
 }
-//
+
 addItem(commitMessage);
